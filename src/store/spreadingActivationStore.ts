6262bcd19a05681c6b0fd9e04c7d9e805fbfe70a
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2025-01-31 12:48:11 +0800

    Create spreadingActivationStore.ts

---
+++ src/store/spreadingActivationStore.ts
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+
+export const spreadingActivationStore = defineStore(
+  "spreading-activation-store",
+  () => {
+    return {};
+  }
+);
